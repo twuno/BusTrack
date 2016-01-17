7798d520dbba546270a9b8efd4800a23c0da8cc8
walter suazo <walters@probit.co>
2016-01-17 02:14:22 -0600

    cambio de crear server

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cors = require('cors')
 var app = express();
 
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 //rutas<s
+
 require('./routes/rutas')(app);
 
 // catch 404 and forward to error handler
