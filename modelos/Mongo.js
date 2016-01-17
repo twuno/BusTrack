32b793b3b02725e6987e65faf4f841b82f4ea787
walter suazo <walters@probit.co>
2016-01-17 02:14:50 -0600

    Conexion Mongo pool

---
+++ modelos/Mongo.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Walter Suazo on 16/01/2016.
+ */
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var mongoUrl="mongodb://terwalito:Shersept03@ds047355.mongolab.com:47355/bustrack";
+var db;
+
+exports.connect = function(callback) {
+  MongoClient.connect(mongoUrl, function(err, database) {
+    if( err )
+    {
+      console.log(err);
+      throw err;
+    }
+    db = database;
+    callback();
+  });
+}
+
+exports.doSomethingWithDatabase = function(callback){
+  // this is using the same db connection
+  var collection = db.collection('geocercas');
+  collection.find({}).toArray(function(err, docs) {
+    // do something
+    if(err)
+    {
+      console.log(err);
+    }else {
+      callback(docs);
+    }
+  });
+};
+
+exports.crear = function(data,callback){
+  // this is using the same db connection
+  var collection = db.collection('geocercas');
+  collection.insertOne({}).toArray(function(err, docs) {
+    // do something
+    if(err)
+    {
+      console.log(err);
+    }else {
+      callback(docs);
+    }
+  });
+};
